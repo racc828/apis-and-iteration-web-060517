a0a9e934bdefc17a413e74935c53920f35b9daf1
github username <github email address>
2017-06-07 16:07:41 -0400

    added a few methods to puts out movies based on character

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,23 +3,40 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
+
+  film_urls_array = find_character_hash(character)["films"]
+
+  films_array = film_urls_array.each_with_object([]) do |film_url, temp_array|
+    film_raw_json = RestClient.get(film_url)
+    film_hash = JSON.parse(film_raw_json)
+    temp_array << film_hash
+  end
+
+  films_array
+
+end
+
+def find_character_hash(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  #parse data
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-end
+
+  #Array of character hashes
+  the_character = character_hash["results"].find do |char_hash|
+    char_hash["name"] == character
+  end
+
+  the_character
+ends
+
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts film["title"]
+  end
+
 end
 
 def show_character_movies(character)
@@ -31,3 +48,6 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+ raw_movies = get_character_movies_from_api("Luke Skywalker")
+
+ parse_character_movies(raw_movies)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,5 +6,5 @@ end
 def get_character_from_user
   puts "Please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
-
+  character = gets.chomp.downcase
 end
