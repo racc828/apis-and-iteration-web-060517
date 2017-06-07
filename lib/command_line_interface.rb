e5842af7bcafce0d2a7998c157df33194f90dfd2
github username <github email address>
2017-06-07 17:58:08 -0400

    ???? "

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,10 @@
 def welcome
-  # puts out a welcome message here!
-  puts "Welcome to star wars!"
+  puts "Welcome to star wars"
 end
 
 def get_character_from_user
+
   puts "Please enter a character"
   character = gets.chomp
+
 end
